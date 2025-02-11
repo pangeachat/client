3f893e8aeb45135b5c97da984cc58ddc38d9c167
Kelrap <kel.raphael3@outlook.com>
2025-02-11 11:44:27 -0500

    Remove route when joining space via code

--- lib/pangea/spaces/controllers/space_controller.dart
+++ lib/pangea/spaces/controllers/space_controller.dart
@@ -1,13 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
-
-import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:go_router/go_router.dart';
-import 'package:matrix/matrix.dart';
-
 import 'package:fluffychat/pangea/common/constants/local.key.dart';
 import 'package:fluffychat/pangea/common/controllers/pangea_controller.dart';
 import 'package:fluffychat/pangea/common/utils/error_handler.dart';
@@ -15,6 +8,12 @@ import 'package:fluffychat/pangea/common/utils/firebase_analytics.dart';
 import 'package:fluffychat/pangea/extensions/pangea_room_extension.dart';
 import 'package:fluffychat/widgets/future_loading_dialog.dart';
 import 'package:fluffychat/widgets/matrix.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:go_router/go_router.dart';
+import 'package:matrix/matrix.dart';
+
 import '../../common/controllers/base_controller.dart';
 
 class ClassController extends BaseController {
@@ -131,7 +130,7 @@ class ClassController extends BaseController {
             );
 
         if (alreadyJoined.isNotEmpty || inFoundClass) {
-          context.go("/rooms/${alreadyJoined.first}/details");
+          context.push("/rooms/${alreadyJoined.first}/details");
           throw L10n.of(context).alreadyInClass;
         }
 
@@ -179,7 +178,7 @@ class ClassController extends BaseController {
         await room.client.waitForRoomInSync(room.id, join: true);
       }
 
-      context.go("/rooms/${room.id}/details");
+      context.push("/rooms/${room.id}/details");
     } catch (e, s) {
       ErrorHandler.logError(
         e: e,
