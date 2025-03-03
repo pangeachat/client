df12154e33d1accd1355f7f89889bba2f02db828
ggurdin <ggurdin@gmail.com>
2025-03-03 14:06:21 -0500

    chore: don't trigger onPaste callback after input text is changed programatically

--- lib/pangea/choreographer/utils/input_paste_listener.dart
+++ lib/pangea/choreographer/utils/input_paste_listener.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:fluffychat/pangea/choreographer/enums/edit_type.dart';
+import 'package:fluffychat/pangea/choreographer/widgets/igc/pangea_text_controller.dart';
+
 class InputPasteListener {
-  final TextEditingController controller;
+  final PangeaTextController controller;
   final VoidCallback onPaste;
 
   String _currentText = '';
 
-  InputPasteListener(this.controller, this.onPaste) {
+  InputPasteListener(
+    this.controller,
+    this.onPaste,
+  ) {
     controller.addListener(() {
+      if (controller.editType != EditType.keyboard) return;
       final difference =
           controller.text.characters.length - _currentText.characters.length;
       if (difference.abs() > 1) onPaste();
