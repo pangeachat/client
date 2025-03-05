7a618ef975dcc9a636e4b2d9bfad223cdfb6fd9b
ggurdin <ggurdin@users.noreply.github.com>
2025-03-05 18:36:59 +0000

    deploy: 50c5fac8dca25151993079bcafcef14596da58d9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2778173865"
+    serviceWorkerVersion: "2313740496"
   }
 });
