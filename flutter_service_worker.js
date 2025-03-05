2945438151c6c56972051eaeca689ab94cd97b13
ggurdin <ggurdin@users.noreply.github.com>
2025-03-05 18:39:41 +0000

    deploy: c9dbbe73b8dd15f8afbc573499c5c27ac4fd89e2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,8 +30,8 @@ const RESOURCES = {"main.dart.js_157.part.js": "76871e4300a86697c3204f0082800ff7
 "main.dart.js_254.part.js": "1eee901aa399fc2c3da6195fe071914c",
 "main.dart.js_212.part.js": "8aedd0a0da2bcd70fe614794551d55b1",
 "main.dart.js_184.part.js": "83e9f0aff8f0aba92dcde551da88b604",
-"index.html": "5649d09a5af726546b8b04ecf762f1b8",
-"/": "5649d09a5af726546b8b04ecf762f1b8",
+"index.html": "bb4b25816fb360b9b972b92c9c5b5d45",
+"/": "bb4b25816fb360b9b972b92c9c5b5d45",
 "main.dart.js_246.part.js": "fa6815fa9bd9383ade135390a3a9a02d",
 "assets/NOTICES": "a8fe814581d185098982d9a0acbb619c",
 "assets/assets/banner_transparent.png": "364e2030f739bf0c7ed1c061c4cb5901",
@@ -105,7 +105,7 @@ const RESOURCES = {"main.dart.js_157.part.js": "76871e4300a86697c3204f0082800ff7
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "main.dart.js_243.part.js": "3bcaca4a1914055f2f679b3354c85e93",
 "main.dart.js_195.part.js": "78a93d6600d64ada036183d816a1508b",
-"main.dart.js": "6fe942966e8b0e6d505e7ce4dd8f0af1",
+"main.dart.js": "b7b0a2fb5b3bc397c5cc981b3e4354fd",
 "main.dart.js_216.part.js": "8d996edcc1d6292a195d18baf4d18b9f",
 "main.dart.js_208.part.js": "bbc1c760bd8bbac56e2fc0b7a67a5371",
 "main.dart.js_2.part.js": "603b68b336f53340312f5012d6817c78",
@@ -144,7 +144,7 @@ const RESOURCES = {"main.dart.js_157.part.js": "76871e4300a86697c3204f0082800ff7
 "main.dart.js_256.part.js": "642980413122ba31126d6a4a2d79c9c7",
 "main.dart.js_201.part.js": "3ce59367f8743f848c577de9036df7af",
 "main.dart.js_1.part.js": "b98219e4659e0b4981ba3628acaff641",
-"flutter_bootstrap.js": "7e7eca7169c99661652eb2069a4f53cb"};
+"flutter_bootstrap.js": "cca8c54c40dbf9224f5214fd35057276"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
