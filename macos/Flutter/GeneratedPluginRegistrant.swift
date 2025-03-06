3854012e785fb11ba3d9a76d6f425cd0fdb35a5f
ggurdin <ggurdin@gmail.com>
2025-03-06 09:11:17 -0500

    fix pubspec

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,6 @@ import desktop_webview_window
 import device_info_plus
 import dynamic_color
 import emoji_picker_flutter
-import file_picker
 import file_selector_macos
 import firebase_analytics
 import firebase_core
@@ -26,7 +25,6 @@ import flutter_webrtc
 import geolocator_apple
 import in_app_purchase_storekit
 import just_audio
-import open_file_mac
 import package_info_plus
 import pasteboard
 import path_provider_foundation
@@ -36,7 +34,7 @@ import rive_common
 import sentry_flutter
 import share_plus
 import shared_preferences_foundation
-import sqflite_darwin
+import sqflite
 import sqlcipher_flutter_libs
 import text_to_speech_macos
 import url_launcher_macos
@@ -53,7 +51,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
-  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
@@ -67,7 +64,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
