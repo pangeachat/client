df9dfeb6422436e6ee6be467a41dbcc4a91f0791
Brord van Wierst <brord@brord.net>
2025-04-21 18:50:25 +0200

    fix for unloaded state

--- lib/pangea/user/controllers/user_controller.dart
+++ lib/pangea/user/controllers/user_controller.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 
 import 'package:collection/collection.dart';
-import 'package:jwt_decode/jwt_decode.dart';
-import 'package:matrix/matrix.dart' as matrix;
-
 import 'package:fluffychat/pangea/common/constants/model_keys.dart';
 import 'package:fluffychat/pangea/common/controllers/base_controller.dart';
 import 'package:fluffychat/pangea/common/controllers/pangea_controller.dart';
@@ -13,6 +10,9 @@ import 'package:fluffychat/pangea/learning_settings/constants/language_constants
 import 'package:fluffychat/pangea/learning_settings/models/language_model.dart';
 import 'package:fluffychat/pangea/learning_settings/utils/p_language_store.dart';
 import 'package:fluffychat/pangea/user/models/profile_model.dart';
+import 'package:jwt_decode/jwt_decode.dart';
+import 'package:matrix/matrix.dart' as matrix;
+
 import '../models/user_model.dart';
 
 /// Controller that manages saving and reading of user/profile information
@@ -281,7 +281,11 @@ class UserController extends BaseController {
           }
         }
       }
+    } else {
+      // Were not loaded yet
+      return false;
     }
+
     return true;
   }
 
