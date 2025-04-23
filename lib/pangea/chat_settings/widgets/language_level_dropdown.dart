220910a91ed6021293ef7d09d34c95b3b5ecb70b
ggurdin <ggurdin@gmail.com>
2025-04-23 15:37:14 -0400

    chore: move language level title and descriptions into enum extension

--- lib/pangea/chat_settings/widgets/language_level_dropdown.dart
+++ lib/pangea/chat_settings/widgets/language_level_dropdown.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
-import 'package:fluffychat/pangea/chat_settings/utils/language_level_copy.dart';
 import 'package:fluffychat/pangea/common/widgets/dropdown_text_button.dart';
 import 'package:fluffychat/pangea/learning_settings/enums/language_level_type_enum.dart';
 
@@ -28,30 +27,18 @@ class LanguageLevelDropdown extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
 
-    final levels = [
-      {'label': l10n.languageLevelPreA1, 'desc': l10n.languageLevelPreA1Desc},
-      {'label': l10n.languageLevelA1, 'desc': l10n.languageLevelA1Desc},
-      {'label': l10n.languageLevelA2, 'desc': l10n.languageLevelA2Desc},
-      {'label': l10n.languageLevelB1, 'desc': l10n.languageLevelB1Desc},
-      {'label': l10n.languageLevelB2, 'desc': l10n.languageLevelB2Desc},
-      {'label': l10n.languageLevelC1, 'desc': l10n.languageLevelC1Desc},
-      {'label': l10n.languageLevelC2, 'desc': l10n.languageLevelC2Desc},
-    ];
-
     return SingleChildScrollView(
       child: DropdownButtonFormField2<LanguageLevelTypeEnum>(
         customButton: initialLevel != null &&
                 LanguageLevelTypeEnum.values.contains(initialLevel)
-            ? CustomDropdownTextButton(
-                text: LanguageLevelTextPicker.languageLevelText(
-                  context,
-                  initialLevel!,
-                ),
-              )
+            ? CustomDropdownTextButton(text: initialLevel!.title(context))
             : null,
         menuItemStyleData: const MenuItemStyleData(
-          padding: EdgeInsets.zero, // Remove default padding
-          height: 75,
+          padding: EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 16.0,
+          ),
+          height: 90.0,
         ),
         decoration: InputDecoration(
           labelText: l10n.cefrLevelLabel,
@@ -67,48 +54,29 @@ class LanguageLevelDropdown extends StatelessWidget {
         ),
         items: LanguageLevelTypeEnum.values
             .map((LanguageLevelTypeEnum levelOption) {
-          final level = levels.firstWhere(
-            (level) =>
-                level['label'] ==
-                LanguageLevelTextPicker.languageLevelText(
-                  context,
-                  levelOption,
-                ),
-            orElse: () => {'label': '', 'desc': ''},
-          );
           return DropdownMenuItem(
             value: levelOption,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Text(
-                    level['label']!,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
+                Text(levelOption.title(context)),
+                Text(
+                  levelOption.description(context),
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    fontSize: 14,
                   ),
-                ),
-                const SizedBox(height: 4),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Text(
-                    level['desc']!,
-                    style: TextStyle(
-                      color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.grey
-                          : Colors.black,
-                      fontSize: 14,
-                    ),
-                    maxLines: null,
-                    overflow: TextOverflow.visible,
-                  ),
+                  maxLines: null,
                 ),
               ],
             ),
           );
         }).toList(),
         onChanged: enabled
-            ? (value) => onChanged?.call(value as LanguageLevelTypeEnum)
+            ? (value) {
+                if (value != null) onChanged?.call(value);
+              }
             : null,
         value: initialLevel,
         validator: validator,
