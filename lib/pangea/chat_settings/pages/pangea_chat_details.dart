020102ee49c2265f7a3d6b8a58a6bef0e58b8145
Kelrap <kel.raphael3@outlook.com>
2025-05-09 14:41:35 -0400

    Center header, DM edits

--- lib/pangea/chat_settings/pages/pangea_chat_details.dart
+++ lib/pangea/chat_settings/pages/pangea_chat_details.dart
@@ -1,11 +1,3 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:flutter_linkify/flutter_linkify.dart';
-import 'package:go_router/go_router.dart';
-import 'package:matrix/matrix.dart';
-
 import 'package:fluffychat/config/themes.dart';
 import 'package:fluffychat/pages/chat_details/chat_details.dart';
 import 'package:fluffychat/pages/chat_details/participant_list_item.dart';
@@ -27,6 +19,12 @@ import 'package:fluffychat/widgets/avatar.dart';
 import 'package:fluffychat/widgets/future_loading_dialog.dart';
 import 'package:fluffychat/widgets/layouts/max_width_body.dart';
 import 'package:fluffychat/widgets/matrix.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:go_router/go_router.dart';
+import 'package:matrix/matrix.dart';
 
 class PangeaChatDetailsView extends StatelessWidget {
   final ChatDetailsController controller;
@@ -104,6 +102,7 @@ class PangeaChatDetailsView extends StatelessWidget {
               controller: controller,
               room: room,
             ),
+            centerTitle: true,
             backgroundColor: theme.appBarTheme.backgroundColor,
           ),
           body: MaxWidthBody(
