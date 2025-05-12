1919888be4510ec44f1e9d016891ecab0b09ccdb
Kelrap <kel.raphael3@outlook.com>
2025-05-12 10:35:37 -0400

    Adds scrolling to friend referral page

--- lib/pangea/chat_settings/widgets/refer_friends_dialog.dart
+++ lib/pangea/chat_settings/widgets/refer_friends_dialog.dart
@@ -1,13 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:matrix/matrix.dart';
-
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/pangea/chat_settings/constants/room_settings_constants.dart';
 import 'package:fluffychat/pangea/common/config/environment.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:matrix/matrix.dart';
 
 class ReferFriendsDialog extends StatelessWidget {
   final Room room;
@@ -31,64 +29,66 @@ class ReferFriendsDialog extends StatelessWidget {
           width: 0.5,
         ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 16.0,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.close),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SizedBox(
-                width: 450,
-                child: CachedNetworkImage(
-                  imageUrl:
-                      "${AppConfig.assetsBaseURL}/${RoomSettingsConstants.referFriendDialogAsset}",
-                  errorWidget: (context, url, error) => const SizedBox(),
-                  placeholder: (context, url) => const Center(
-                    child: CircularProgressIndicator.adaptive(),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 16.0,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.close),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  width: 450,
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        "${AppConfig.assetsBaseURL}/${RoomSettingsConstants.referFriendDialogAsset}",
+                    errorWidget: (context, url, error) => const SizedBox(),
+                    placeholder: (context, url) => const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Text(
-            L10n.of(context).referFriendDialogTitle,
-            style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-          ),
-          Text(
-            L10n.of(context).referFriendDialogDesc,
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          Material(
-            color: Colors.transparent, // Keeps the `Container`'s background
-            child: ListTile(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24.0),
-                side: BorderSide(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              ),
-              title: Text(
-                inviteLink,
-                overflow: TextOverflow.ellipsis,
-              ),
-              trailing: const Icon(Icons.copy_outlined),
-              onTap: () async {
-                Clipboard.setData(
-                  ClipboardData(text: inviteLink),
-                );
-              },
+            Text(
+              L10n.of(context).referFriendDialogTitle,
+              style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
             ),
-          ),
-        ],
+            Text(
+              L10n.of(context).referFriendDialogDesc,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Material(
+              color: Colors.transparent, // Keeps the `Container`'s background
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24.0),
+                  side: BorderSide(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                title: Text(
+                  inviteLink,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: const Icon(Icons.copy_outlined),
+                onTap: () async {
+                  Clipboard.setData(
+                    ClipboardData(text: inviteLink),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
