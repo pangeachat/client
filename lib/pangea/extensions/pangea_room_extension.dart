efc3b699feca6e8bef8dc552f241c9da882d1d59
ggurdin <ggurdin@gmail.com>
2025-06-17 10:05:09 -0400

    reference for getting saved level up summary from analytics room

--- lib/pangea/extensions/pangea_room_extension.dart
+++ lib/pangea/extensions/pangea_room_extension.dart
@@ -24,6 +24,7 @@ import 'package:fluffychat/pangea/chat_settings/constants/pangea_room_types.dart
 import 'package:fluffychat/pangea/chat_settings/models/bot_options_model.dart';
 import 'package:fluffychat/pangea/common/constants/model_keys.dart';
 import 'package:fluffychat/pangea/common/utils/error_handler.dart';
+import 'package:fluffychat/pangea/constructs/construct_repo.dart';
 import 'package:fluffychat/pangea/events/models/tokens_event_content_model.dart';
 import 'package:fluffychat/pangea/extensions/pangea_room_extension.dart';
 import 'package:fluffychat/pangea/spaces/constants/space_constants.dart';
