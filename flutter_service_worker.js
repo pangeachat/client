7b2a1086e238a5bc7a183b05e404dc1240ff4c2d
ggurdin <ggurdin@users.noreply.github.com>
2025-07-16 14:01:32 +0000

    deploy: 8e5dc610f81dafc4db807317b005cd0d66ee8025

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -82,7 +82,7 @@ const RESOURCES = {"main.dart.js_223.part.js": "09c25aa042676ef6c3d33af9c720dc64
 "main.dart.js_217.part.js": "465e58d742b2d1361e958c68a49a18e8",
 "main.dart.js_235.part.js": "0c25f33d9e0db1da1b0cf26912c91e45",
 "main.dart.js_221.part.js": "02e6b42ffd676af702e4f9721ae2ce78",
-"flutter_bootstrap.js": "70b114c95245ef83e62bbdae8b939f14",
+"flutter_bootstrap.js": "d1d0822900f7acf0ec7fb80e15021549",
 "main.dart.js_264.part.js": "9303c67473d173aca09d36a4b42c668f",
 "main.dart.js_213.part.js": "b6c2a28971cd20e157b3b168355b16ab",
 "main.dart.js_206.part.js": "36c058c980b9063b36b3e16e3ccac5f1",
@@ -110,8 +110,8 @@ const RESOURCES = {"main.dart.js_223.part.js": "09c25aa042676ef6c3d33af9c720dc64
 "splash/img/dark-4x.png": "ead87864be6b8f2f3efbb04acd30549d",
 "splash/img/light-3x.png": "16878fb08884c14b4d8971feec70a8b3",
 "splash/style.css": "ffbfc8e81bf12699a69e56fed40c3d90",
-"index.html": "50f6d6bbfdac1c7610d3bca74d048c07",
-"/": "50f6d6bbfdac1c7610d3bca74d048c07",
+"index.html": "a828da57a61d8002a70582c505402c64",
+"/": "a828da57a61d8002a70582c505402c64",
 "main.dart.js_243.part.js": "4983edfa874f843895cd29e1ff7a09b9",
 "main.dart.js_242.part.js": "924955db722d6312e060ed4a1097fc3d",
 "favicon.png": "37d87985849bc680fe47a9330c3ea67e",
@@ -136,7 +136,7 @@ const RESOURCES = {"main.dart.js_223.part.js": "09c25aa042676ef6c3d33af9c720dc64
 "main.dart.js_240.part.js": "06944d5a4aff861e1b629706983eca6a",
 "main.dart.js_262.part.js": "1aa6785345f73914bbe8a844de9f5fdc",
 "main.dart.js_19.part.js": "375b70cd53006a8035a961ab2cb5a351",
-"main.dart.js": "cd39366d66503f6e1a8affe7de071200",
+"main.dart.js": "501112c93a856704fd82dd662f7f9448",
 "main.dart.js_257.part.js": "b733627442bc179ffc5af7cf261cafa0"};
 // The application shell files that are downloaded before a service worker can
 // start.
