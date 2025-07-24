1008539e44091e4a946edec134759c29451239fd
Kelrap <kel.raphael3@outlook.com>
2025-07-24 12:57:59 -0400

    Edited unit test to use ChoreoEdit instead of bsdiff

--- test/pangea/choreo_record_test.dart
+++ test/pangea/choreo_record_test.dart
@@ -1,24 +1,21 @@
-import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 
-import 'package:bsdiff/bsdiff.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fluffychat/pangea/choreographer/models/choreo_edit.dart';
 
 void main() async {
   group("Optimized choreo record tests", () {
-    test("Test that data saved in patches can be accurately retrieved", () {
-      const String originalText = "a";
-      const String editedText = "a";
-      debugPrint('Original message length: ${originalText.length}');
-      debugPrint('Edited message length: ${editedText.length}');
+    test("Test that data saved in via choreo_edit can be accurately retrieved",
+        () {
+      const String originalText = "Aqui esta un menaje";
+      const String editedText = "Aqui esta un mensaje";
 
-      final Uint8List edits =
-          bsdiff(utf8.encode(originalText), utf8.encode(editedText));
+      final ChoreoEdit edits = ChoreoEdit.fromText(
+          originalText: originalText, editedText: editedText);
       debugPrint('Edits length: ${edits.length}');
 
-      final retrievedText =
-          utf8.decode(bspatch(utf8.encode(originalText), edits));
+      final retrievedText = edits.editedText(originalText);
       debugPrint('Retrieved text: $retrievedText');
       expect(
         retrievedText,
