abd33d2ccb97d2193f3c9422121222a94728add5
ggurdin <ggurdin@gmail.com>
2025-07-24 15:29:30 -0400

    chore: formatting

--- lib/pages/chat/events/message_reactions.dart
+++ lib/pages/chat/events/message_reactions.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
+
 import 'package:collection/collection.dart' show IterableExtension;
+import 'package:matrix/matrix.dart';
+
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/config/themes.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
@@ -6,8 +10,6 @@ import 'package:fluffychat/widgets/avatar.dart';
 import 'package:fluffychat/widgets/future_loading_dialog.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:fluffychat/widgets/mxc_image.dart';
-import 'package:flutter/material.dart';
-import 'package:matrix/matrix.dart';
 
 class MessageReactions extends StatelessWidget {
   final Event event;
--- lib/pangea/toolbar/widgets/overlay_center_content.dart
+++ lib/pangea/toolbar/widgets/overlay_center_content.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:matrix/matrix.dart';
+
 import 'package:fluffychat/config/themes.dart';
 import 'package:fluffychat/pages/chat/chat.dart';
 import 'package:fluffychat/pages/chat/events/pangea_message_reactions.dart';
@@ -5,8 +9,6 @@ import 'package:fluffychat/pangea/toolbar/enums/reading_assistance_mode_enum.dar
 import 'package:fluffychat/pangea/toolbar/widgets/measure_render_box.dart';
 import 'package:fluffychat/pangea/toolbar/widgets/message_selection_overlay.dart';
 import 'package:fluffychat/pangea/toolbar/widgets/overlay_message.dart';
-import 'package:flutter/material.dart';
-import 'package:matrix/matrix.dart';
 
 class OverlayCenterContent extends StatelessWidget {
   final Event event;
