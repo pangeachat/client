2f855941f7e88145544cb6939416acf89f78c0cf
ggurdin <ggurdin@gmail.com>
2025-08-11 09:47:15 -0400

    chore: wrap button in find you people page on narrow two-column screen widths

--- lib/pangea/find_your_people/find_your_people_view.dart
+++ lib/pangea/find_your_people/find_your_people_view.dart
@@ -20,12 +20,10 @@ class FindYourPeopleView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    // Treats web screens without enough space like mobile screens
-    final isWideColumnMode = MediaQuery.of(context).size.width >
-        FluffyThemes.columnWidth * 2.5 + FluffyThemes.navRailWidth;
+    final isColumnMode = FluffyThemes.isColumnMode(context);
 
     return Scaffold(
-      appBar: isWideColumnMode
+      appBar: isColumnMode
           ? null
           : AppBar(
               leading: IconButton(
@@ -66,7 +64,7 @@ class FindYourPeopleView extends StatelessWidget {
                 ),
               ],
             ),
-      floatingActionButton: isWideColumnMode
+      floatingActionButton: isColumnMode
           ? null
           : FloatingActionButton.extended(
               onPressed: () => context.push('/rooms/newspace'),
@@ -77,7 +75,7 @@ class FindYourPeopleView extends StatelessWidget {
               ),
             ),
       body: Padding(
-        padding: isWideColumnMode
+        padding: isColumnMode
             ? const EdgeInsets.symmetric(
                 horizontal: 24.0,
                 vertical: 20.0,
@@ -87,7 +85,7 @@ class FindYourPeopleView extends StatelessWidget {
           spacing: 16.0,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            if (isWideColumnMode)
+            if (isColumnMode)
               Padding(
                 padding: const EdgeInsets.symmetric(
                   vertical: 16.0,
@@ -100,101 +98,131 @@ class FindYourPeopleView extends StatelessWidget {
               ),
             Expanded(
               child: Column(
-                spacing: isWideColumnMode ? 32.0 : 16.0,
+                spacing: isColumnMode ? 32.0 : 16.0,
                 children: [
-                  Container(
-                    height: 48.0,
-                    padding: isWideColumnMode
-                        ? const EdgeInsets.symmetric(horizontal: 12)
-                        : null,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      spacing: 10,
-                      children: [
-                        Expanded(
-                          child: SizedBox(
-                            height: 40.0,
-                            child: TextField(
-                              controller: controller.searchController,
-                              onChanged: controller.onSearchEnter,
-                              textInputAction: TextInputAction.search,
-                              decoration: InputDecoration(
-                                filled: !isWideColumnMode,
-                                fillColor: isWideColumnMode
-                                    ? null
-                                    : theme.colorScheme.secondaryContainer,
-                                border: OutlineInputBorder(
-                                  borderSide: isWideColumnMode
-                                      ? const BorderSide()
-                                      : BorderSide.none,
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                contentPadding:
-                                    const EdgeInsets.fromLTRB(0, 0, 20.0, 0),
-                                hintText: L10n.of(context).findYourPeople,
-                                hintStyle: TextStyle(
-                                  color: theme.colorScheme.onPrimaryContainer,
-                                  fontWeight: FontWeight.normal,
-                                  fontSize: 16.0,
-                                ),
-                                floatingLabelBehavior:
-                                    FloatingLabelBehavior.never,
-                                prefixIcon: IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.search_outlined,
-                                    color: theme.colorScheme.onPrimaryContainer,
+                  LayoutBuilder(
+                    builder: (context, constraints) {
+                      return SizedBox(
+                        width: constraints.maxWidth,
+                        child: Wrap(
+                          alignment: WrapAlignment.spaceBetween,
+                          spacing: 10,
+                          runSpacing: 10,
+                          children: [
+                            ConstrainedBox(
+                              constraints: isColumnMode
+                                  ? const BoxConstraints(
+                                      minWidth: 200.0,
+                                      maxWidth: 400.0,
+                                    )
+                                  : BoxConstraints(
+                                      maxWidth:
+                                          MediaQuery.of(context).size.width -
+                                              24.0,
+                                    ),
+                              child: SizedBox(
+                                height: 40.0,
+                                width: isColumnMode ? 300.0 : null,
+                                child: TextField(
+                                  controller: controller.searchController,
+                                  onChanged: controller.onSearchEnter,
+                                  textInputAction: TextInputAction.search,
+                                  decoration: InputDecoration(
+                                    filled: !isColumnMode,
+                                    fillColor: isColumnMode
+                                        ? null
+                                        : theme.colorScheme.secondaryContainer,
+                                    border: OutlineInputBorder(
+                                      borderSide: isColumnMode
+                                          ? const BorderSide()
+                                          : BorderSide.none,
+                                      borderRadius: BorderRadius.circular(100),
+                                    ),
+                                    contentPadding: const EdgeInsets.fromLTRB(
+                                      0,
+                                      0,
+                                      20.0,
+                                      0,
+                                    ),
+                                    hintText: L10n.of(context).findYourPeople,
+                                    hintStyle: TextStyle(
+                                      color:
+                                          theme.colorScheme.onPrimaryContainer,
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 16.0,
+                                    ),
+                                    floatingLabelBehavior:
+                                        FloatingLabelBehavior.never,
+                                    prefixIcon: IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Icons.search_outlined,
+                                        color: theme
+                                            .colorScheme.onPrimaryContainer,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
+                            if (isColumnMode)
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextButton(
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.join_full,
+                                          color: theme
+                                              .colorScheme.onPrimaryContainer,
+                                          size: 24.0,
+                                        ),
+                                        const SizedBox(width: 8.0),
+                                        Text(
+                                          L10n.of(context).joinWithCode,
+                                          style: TextStyle(
+                                            color: theme
+                                                .colorScheme.onPrimaryContainer,
+                                            fontSize: 16.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    onPressed: () =>
+                                        SpaceCodeUtil.joinWithSpaceCodeDialog(
+                                      context,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.add_box_outlined,
+                                          color: theme
+                                              .colorScheme.onPrimaryContainer,
+                                          size: 24.0,
+                                        ),
+                                        const SizedBox(width: 8.0),
+                                        Text(
+                                          L10n.of(context).createYourSpace,
+                                          style: TextStyle(
+                                            color: theme
+                                                .colorScheme.onPrimaryContainer,
+                                            fontSize: 16.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    onPressed: () =>
+                                        context.push('/rooms/newspace'),
+                                  ),
+                                ],
+                              ),
+                          ],
                         ),
-                        if (isWideColumnMode)
-                          TextButton(
-                            child: Row(
-                              children: [
-                                Icon(
-                                  Icons.join_full,
-                                  color: theme.colorScheme.onPrimaryContainer,
-                                  size: 24.0,
-                                ),
-                                const SizedBox(width: 8.0),
-                                Text(
-                                  L10n.of(context).joinWithCode,
-                                  style: TextStyle(
-                                    color: theme.colorScheme.onPrimaryContainer,
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            onPressed: () =>
-                                SpaceCodeUtil.joinWithSpaceCodeDialog(context),
-                          ),
-                        if (isWideColumnMode)
-                          TextButton(
-                            child: Row(
-                              children: [
-                                Icon(
-                                  Icons.add_box_outlined,
-                                  color: theme.colorScheme.onPrimaryContainer,
-                                  size: 24.0,
-                                ),
-                                const SizedBox(width: 8.0),
-                                Text(
-                                  L10n.of(context).createYourSpace,
-                                  style: TextStyle(
-                                    color: theme.colorScheme.onPrimaryContainer,
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            onPressed: () => context.push('/rooms/newspace'),
-                          ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
                   controller.error != null
                       ? Column(
@@ -223,7 +251,7 @@ class FindYourPeopleView extends StatelessWidget {
                                       final space =
                                           controller.spaceItems[index];
                                       return Padding(
-                                        padding: isWideColumnMode
+                                        padding: isColumnMode
                                             ? const EdgeInsets.only(
                                                 bottom: 32.0,
                                               )
