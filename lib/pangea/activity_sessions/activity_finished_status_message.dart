8e847322e5660a2b69eee5303fbf07b292db3257
ggurdin <ggurdin@gmail.com>
2025-08-12 10:27:23 -0400

    chore: simplify activity summary display, add activity summary widget to chat event list

--- lib/pangea/activity_sessions/activity_finished_status_message.dart
+++ lib/pangea/activity_sessions/activity_finished_status_message.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:collection/collection.dart';
 import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
@@ -14,7 +13,6 @@ import 'package:fluffychat/pangea/activity_sessions/activity_role_model.dart';
 import 'package:fluffychat/pangea/activity_sessions/activity_room_extension.dart';
 import 'package:fluffychat/widgets/future_loading_dialog.dart';
 import 'package:fluffychat/widgets/matrix.dart';
-import 'package:fluffychat/widgets/mxc_image.dart';
 
 class ActivityFinishedStatusMessage extends StatefulWidget {
   final Room room;
@@ -32,7 +30,6 @@ class ActivityFinishedStatusMessage extends StatefulWidget {
 class ActivityFinishedStatusMessageState
     extends State<ActivityFinishedStatusMessage> {
   ActivityRoleModel? _highlightedRole;
-  bool _expanded = true;
 
   @override
   void initState() {
@@ -52,21 +49,17 @@ class ActivityFinishedStatusMessageState
 
   Map<String, ActivityRole> get _roles => widget.room.activityPlan?.roles ?? {};
 
-  void _setExpanded(bool expanded) {
-    if (mounted) setState(() => _expanded = expanded);
-  }
-
   int get _hightlightedRoleIndex {
     if (_highlightedRole == null) {
       return -1; // No highlighted role
     }
-    return rolesWithSummaries.indexOf(_highlightedRole!);
+    return _rolesWithSummaries.indexOf(_highlightedRole!);
   }
 
   void _setDefaultHighlightedRole() {
     if (_hightlightedRoleIndex >= 0) return;
 
-    final roles = rolesWithSummaries;
+    final roles = _rolesWithSummaries;
     _highlightedRole = roles.firstWhereOrNull(
       (r) => r.userId == widget.room.client.userID,
     );
@@ -82,32 +75,13 @@ class ActivityFinishedStatusMessageState
     if (mounted) setState(() => _highlightedRole = role);
   }
 
-  bool get _canMoveLeft =>
-      _hightlightedRoleIndex > 0 && _highlightedRole != null;
-
-  bool get _canMoveRight =>
-      _hightlightedRoleIndex < rolesWithSummaries.length - 1 &&
-      _highlightedRole != null;
-
-  void _moveLeft() {
-    if (_hightlightedRoleIndex > 0) {
-      _highlightRole(rolesWithSummaries[_hightlightedRoleIndex - 1]);
-    }
-  }
-
-  void _moveRight() {
-    if (_hightlightedRoleIndex < rolesWithSummaries.length - 1) {
-      _highlightRole(rolesWithSummaries[_hightlightedRoleIndex + 1]);
-    }
-  }
-
   Future<void> _archiveToAnalytics() async {
     await widget.room.archiveActivity();
     await MatrixState.pangeaController.putAnalytics
         .sendActivityAnalytics(widget.room.id);
   }
 
-  List<ActivityRoleModel> get rolesWithSummaries {
+  List<ActivityRoleModel> get _rolesWithSummaries {
     if (widget.room.activitySummary?.summary == null) {
       return <ActivityRoleModel>[];
     }
@@ -123,11 +97,13 @@ class ActivityFinishedStatusMessageState
 
   @override
   Widget build(BuildContext context) {
+    if (!widget.room.showActivityChatUI || !widget.room.activityIsFinished) {
+      return const SizedBox.shrink();
+    }
+
     final summary = widget.room.activitySummary;
-    final imageURL = widget.room.activityPlan!.imageURL;
 
     final theme = Theme.of(context);
-    final isColumnMode = MediaQuery.of(context).size.width < 600;
 
     final user = widget.room.getParticipants().firstWhereOrNull(
           (u) => u.id == _highlightedRole?.userId,
@@ -140,175 +116,138 @@ class ActivityFinishedStatusMessageState
 
     return AnimatedSize(
       duration: FluffyThemes.animationDuration,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: _expanded
-            ? [
-                if (summary?.summary != null) ...[
-                  IconButton(
-                    icon: Icon(
-                      Icons.expand_more,
-                      color: theme.colorScheme.onSurfaceVariant,
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          constraints: const BoxConstraints(
+            maxWidth: FluffyThemes.columnWidth * 1.5,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (summary?.summary != null) ...[
+                Text(
+                  L10n.of(context).activityFinishedMessage,
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Text(
+                    summary!.summary!.summary,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 14.0,
                     ),
-                    onPressed: () => _setExpanded(!_expanded),
                   ),
-                  const SizedBox(height: 8.0),
-                  if (imageURL != null)
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: imageURL.startsWith("mxc")
-                          ? MxcImage(
-                              uri: Uri.parse(imageURL),
-                              width: 100.0,
-                              height: 100.0,
-                              cacheKey: widget.room.activityPlan!.bookmarkId,
-                              fit: BoxFit.cover,
-                            )
-                          : CachedNetworkImage(
-                              imageUrl: imageURL,
-                              fit: BoxFit.cover,
-                              width: 100.0,
-                              height: 100.0,
-                              placeholder: (
-                                context,
-                                url,
-                              ) =>
-                                  const Center(
-                                child: CircularProgressIndicator(),
-                              ),
-                              errorWidget: (
-                                context,
-                                url,
-                                error,
-                              ) =>
-                                  const SizedBox(),
-                            ),
-                    ),
+                ),
+                if (_highlightedRole != null && userSummary != null)
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Text(
-                      summary!.summary!.summary,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: isColumnMode ? 16.0 : 12.0,
-                      ),
-                    ),
-                  ),
-                  if (_highlightedRole != null && userSummary != null)
-                    ActivityResultsCarousel(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ActivityResultsCarousel(
                       selectedRole: _highlightedRole!,
-                      moveLeft: _canMoveLeft ? _moveLeft : null,
-                      moveRight: _canMoveRight ? _moveRight : null,
                       user: user,
                       summary: userSummary,
                     ),
-                  const SizedBox(height: 8.0),
-                  Wrap(
-                    alignment: WrapAlignment.center,
-                    spacing: 12.0,
-                    runSpacing: 12.0,
-                    children: rolesWithSummaries.map(
-                      (role) {
-                        final user =
-                            widget.room.getParticipants().firstWhereOrNull(
-                                  (u) => u.id == role.userId,
-                                );
+                  ),
+                const SizedBox(height: 8.0),
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  spacing: 12.0,
+                  runSpacing: 12.0,
+                  children: _rolesWithSummaries.map(
+                    (role) {
+                      final user =
+                          widget.room.getParticipants().firstWhereOrNull(
+                                (u) => u.id == role.userId,
+                              );
 
-                        return ActivityParticipantIndicator(
-                          availableRole: _roles[role.id]!,
-                          avatarUrl: _roles[role.id]?.avatarUrl ??
-                              user?.avatarUrl?.toString(),
-                          onTap: _highlightedRole == role
-                              ? null
-                              : () => _highlightRole(role),
-                          assignedRole: role,
-                          selected: _highlightedRole == role,
-                        );
-                      },
-                    ).toList(),
-                  ),
-                  const SizedBox(height: 20.0),
-                ] else if (summary?.isLoading ?? false)
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      spacing: 8.0,
-                      children: [
-                        const CircularProgressIndicator.adaptive(),
-                        Text(L10n.of(context).loadingActivitySummary),
-                      ],
-                    ),
-                  )
-                else if (summary?.hasError ?? false)
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      spacing: 8.0,
-                      children: [
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(
-                              Icons.school_outlined,
-                              size: 24.0,
-                            ),
-                            const SizedBox(width: 8),
-                            Flexible(
-                              child: Text(
-                                L10n.of(context).activitySummaryError,
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ],
-                        ),
-                        TextButton(
-                          onPressed: () => widget.room.fetchSummaries(),
-                          child: Text(L10n.of(context).requestSummaries),
-                        ),
-                      ],
-                    ),
-                  ),
-                if (!widget.room.isHiddenActivityRoom)
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12.0,
-                        vertical: 8.0,
-                      ),
-                      foregroundColor: theme.colorScheme.onPrimaryContainer,
-                      backgroundColor: theme.colorScheme.primaryContainer,
-                    ),
-                    onPressed: () async {
-                      final resp = await showFutureLoadingDialog(
-                        context: context,
-                        future: _archiveToAnalytics,
+                      return ActivityParticipantIndicator(
+                        availableRole: _roles[role.id]!,
+                        avatarUrl: _roles[role.id]?.avatarUrl ??
+                            user?.avatarUrl?.toString(),
+                        onTap: _highlightedRole == role
+                            ? null
+                            : () => _highlightRole(role),
+                        assignedRole: role,
+                        selected: _highlightedRole == role,
                       );
-
-                      if (!resp.isError) {
-                        context.go(
-                          "/rooms/analytics?mode=activities",
-                        );
-                      }
                     },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(L10n.of(context).archiveToAnalytics),
-                      ],
-                    ),
+                  ).toList(),
+                ),
+                const SizedBox(height: 20.0),
+              ] else if (summary?.isLoading ?? false)
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    spacing: 8.0,
+                    children: [
+                      const CircularProgressIndicator.adaptive(),
+                      Text(L10n.of(context).loadingActivitySummary),
+                    ],
                   ),
-              ]
-            : [
-                if (summary != null)
-                  IconButton(
-                    icon: Icon(
-                      Icons.expand_less,
-                      color: theme.colorScheme.onSurfaceVariant,
-                    ),
-                    onPressed: () => _setExpanded(!_expanded),
+                )
+              else if (summary?.hasError ?? false)
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    spacing: 8.0,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(
+                            Icons.school_outlined,
+                            size: 24.0,
+                          ),
+                          const SizedBox(width: 8),
+                          Flexible(
+                            child: Text(
+                              L10n.of(context).activitySummaryError,
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () => widget.room.fetchSummaries(),
+                        child: Text(L10n.of(context).requestSummaries),
+                      ),
+                    ],
                   ),
-              ],
+                ),
+              if (!widget.room.isHiddenActivityRoom)
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12.0,
+                      vertical: 8.0,
+                    ),
+                    foregroundColor: theme.colorScheme.onPrimaryContainer,
+                    backgroundColor: theme.colorScheme.primaryContainer,
+                  ),
+                  onPressed: () async {
+                    final resp = await showFutureLoadingDialog(
+                      context: context,
+                      future: _archiveToAnalytics,
+                    );
+
+                    if (!resp.isError) {
+                      context.go(
+                        "/rooms/analytics?mode=activities",
+                      );
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(L10n.of(context).archiveToAnalytics),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
