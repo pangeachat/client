ee80e1e6b5c89b1791d082cf06ed0e5a6edf6114
ggurdin <ggurdin@gmail.com>
2025-08-25 10:59:06 -0400

    remove raised effect from activity suggestion cards

--- lib/pangea/activity_suggestions/activity_suggestion_card.dart
+++ lib/pangea/activity_suggestions/activity_suggestion_card.dart
@@ -4,7 +4,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:fluffychat/pangea/activity_planner/activity_plan_model.dart';
 import 'package:fluffychat/pangea/activity_planner/activity_planner_builder.dart';
-import 'package:fluffychat/pangea/common/widgets/pressable_button.dart';
 import 'package:fluffychat/widgets/mxc_image.dart';
 
 class ActivitySuggestionCard extends StatelessWidget {
@@ -33,145 +32,125 @@ class ActivitySuggestionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return PressableButton(
-      onPressed: onPressed,
-      borderRadius: BorderRadius.circular(24.0),
-      color: theme.brightness == Brightness.dark
-          ? theme.colorScheme.primary
-          : theme.colorScheme.surfaceContainerHighest,
-      colorFactor: theme.brightness == Brightness.dark ? 0.6 : 0.2,
-      buttonHeight: 2.0,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(24.0),
-        ),
-        height: height,
-        width: width,
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: theme.colorScheme.surfaceContainer,
-                borderRadius: BorderRadius.circular(24.0),
-              ),
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  height: width,
-                  width: width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(24.0),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(24.0),
-                    child: activity.imageURL != null
-                        ? activity.imageURL!.startsWith("mxc")
-                            ? MxcImage(
-                                uri: Uri.parse(activity.imageURL!),
-                                width: width,
-                                height: width,
-                                cacheKey: activity.bookmarkId,
-                                fit: BoxFit.cover,
-                              )
-                            : CachedNetworkImage(
-                                imageUrl: activity.imageURL!,
-                                placeholder: (context, url) => const Center(
-                                  child: CircularProgressIndicator(),
-                                ),
-                                errorWidget: (context, url, error) =>
-                                    const SizedBox(),
-                                fit: BoxFit.cover,
-                              )
-                        : null,
-                  ),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24.0),
+          ),
+          height: height,
+          width: width,
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.surfaceContainer,
+                  borderRadius: BorderRadius.circular(24.0),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Flexible(
-                              child: Text(
-                                activity.title,
-                                style: TextStyle(
-                                  fontSize: fontSize,
-                                ),
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          spacing: 8.0,
-                          children: [
-                            if (activity.req.mode.isNotEmpty)
-                              Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: Flexible(
-                                  child: Text(
-                                    activity.req.mode,
-                                    style: fontSizeSmall != null
-                                        ? TextStyle(fontSize: fontSizeSmall)
-                                        : theme.textTheme.labelSmall,
-                                    overflow: TextOverflow.ellipsis,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: width,
+                    width: width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(24.0),
+                      child: activity.imageURL != null
+                          ? activity.imageURL!.startsWith("mxc")
+                              ? MxcImage(
+                                  uri: Uri.parse(activity.imageURL!),
+                                  width: width,
+                                  height: width,
+                                  cacheKey: activity.bookmarkId,
+                                  fit: BoxFit.cover,
+                                )
+                              : CachedNetworkImage(
+                                  imageUrl: activity.imageURL!,
+                                  placeholder: (context, url) => const Center(
+                                    child: CircularProgressIndicator(),
                                   ),
-                                ),
-                              ),
-                            Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                spacing: 4.0,
-                                children: [
-                                  Icon(
-                                    Icons.group_outlined,
-                                    size: iconSize ?? 12.0,
-                                  ),
-                                  Text(
-                                    "${activity.req.numberOfParticipants}",
-                                    style: fontSizeSmall != null
-                                        ? TextStyle(fontSize: fontSizeSmall)
-                                        : theme.textTheme.labelSmall,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                                  errorWidget: (context, url, error) =>
+                                      const SizedBox(),
+                                  fit: BoxFit.cover,
+                                )
+                          : null,
                     ),
                   ),
-                ),
-              ],
-            ),
-            //     Positioned(
-            //       top: 4.0,
-            //       right: 4.0,
-            //       child: IconButton(
-            //         icon: Icon(
-            //           controller.isBookmarked ? Icons.save : Icons.save_outlined,
-            //           color: Theme.of(context).colorScheme.onPrimaryContainer,
-            //         ),
-            //         onPressed: controller.toggleBookmarkedActivity,
-            //         style: IconButton.styleFrom(
-            //           backgroundColor: Theme.of(context)
-            //               .colorScheme
-            //               .primaryContainer
-            //               .withAlpha(180),
-            //         ),
-            //       ),
-            //     ),
-          ],
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  activity.title,
+                                  style: TextStyle(
+                                    fontSize: fontSize,
+                                  ),
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            spacing: 8.0,
+                            children: [
+                              if (activity.req.mode.isNotEmpty)
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Flexible(
+                                    child: Text(
+                                      activity.req.mode,
+                                      style: fontSizeSmall != null
+                                          ? TextStyle(fontSize: fontSizeSmall)
+                                          : theme.textTheme.labelSmall,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ),
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  spacing: 4.0,
+                                  children: [
+                                    Icon(
+                                      Icons.group_outlined,
+                                      size: iconSize ?? 12.0,
+                                    ),
+                                    Text(
+                                      "${activity.req.numberOfParticipants}",
+                                      style: fontSizeSmall != null
+                                          ? TextStyle(fontSize: fontSizeSmall)
+                                          : theme.textTheme.labelSmall,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
