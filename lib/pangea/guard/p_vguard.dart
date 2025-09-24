89fd6a0f1d26239582afcbe9a9fdaab95453f760
ggurdin <ggurdin@gmail.com>
2025-09-24 11:51:28 -0400

    feat: new onboarding flow

--- lib/pangea/guard/p_vguard.dart
+++ lib/pangea/guard/p_vguard.dart
@@ -20,9 +20,13 @@ class PAuthGaurd {
           .widget
           .clients
           .any((client) => client.isLogged())) {
-        final bool dobIsSet =
-            await pController!.userController.isUserDataAvailableAndL2Set;
-        return dobIsSet ? '/rooms' : '/user_age';
+        final bool hasSetL2 = await pController!.userController.isUserL2Set;
+        final langCode = state.pathParameters['langcode'];
+        return hasSetL2
+            ? null
+            : langCode != null
+                ? '/course/$langCode'
+                : '/course';
       }
       return null;
     } else {
@@ -43,114 +47,16 @@ class PAuthGaurd {
           .any((client) => client.isLogged())) {
         return '/home';
       }
-      final bool dobIsSet =
-          await pController!.userController.isUserDataAvailableAndL2Set;
-      return dobIsSet ? null : '/user_age';
+      final bool hasSetL2 = await pController!.userController.isUserL2Set;
+      final langCode = state.pathParameters['langcode'];
+      return hasSetL2 || (state.fullPath?.contains('course') ?? false)
+          ? null
+          : langCode != null
+              ? '/course/$langCode'
+              : '/course';
     } else {
       debugPrint("controller is null in pguard check");
       return Matrix.of(context).client.isLogged() ? null : '/home';
     }
   }
-
-  // static const defaultRoute = '/home';
-
-  // static Future<void> onPublicEnter() async {
-  // final bool setDob =
-  //     await pController!.userController.isUserDataAvailableAndDateOfBirthSet;
-  //   if (_isLogged != null && _isLogged! && setDob) {
-  //     vRedirector.to('/rooms');
-  //   }
-  // }
-
-  // static Future<void> onPublicUpdate(VRedirector vRedirector) async {
-  //   final bool setDob =
-  //       await pController!.userController.isUserDataAvailableAndDateOfBirthSet;
-  //   if (_isLogged != null && _isLogged! && setDob) {
-  //     vRedirector.to('/rooms');
-  //   }
-  //   bool oldHaveParms = false;
-
-  //   final bool haveData = vRedirector.previousVRouterData != null;
-  //   if (haveData) {
-  //     final bool isPublicRoute =
-  //         vRedirector.newVRouterData!.url!.startsWith(defaultRoute);
-  //     if (!isPublicRoute) {
-  //       return;
-  //     }
-  //     oldHaveParms =
-  //         vRedirector.previousVRouterData!.queryParameters.isNotEmpty;
-  //     if (oldHaveParms) {
-  //       if (vRedirector.newVRouterData!.queryParameters.isEmpty) {
-  //         vRedirector.to(
-  //           vRedirector.toUrl!,
-  //           queryParameters: vRedirector.previousVRouterData!.queryParameters,
-  //         );
-  //       }
-  //     }
-  //   }
-
-  //   return;
-  // }
-
-  // static Future<void> onPublicLeave(
-  //   VRedirector vRedirector,
-  //   Function(Map<String, String> onLeave) callback,
-  // ) async {
-  //   final bool haveData = vRedirector.previousVRouterData != null;
-
-  //   if (haveData) {
-  //     try {
-  //       if (vRedirector.previousVRouterData!.queryParameters['redirect'] ==
-  //           'true') {
-  //         if (!isPublicLeaving) {
-  //           isPublicLeaving = true;
-  //           vRedirector.to(
-  //             vRedirector.previousVRouterData!.queryParameters['redirectPath']!,
-  //           );
-  //         }
-  //       }
-  //     } catch (e, s) {
-  //       ErrorHandler.logError(e: e, s: s);
-  //     }
-  //   }
-  //   return;
-  // }
-
-  // static Future<void> onPrivateUpdate(VRedirector vRedirector) async {
-  //   if (_isLogged == null) {
-  //     return;
-  //   }
-  //   final Map<String, String> redirectParm = {};
-  //   final bool haveData = vRedirector.newVRouterData != null;
-  //   if (haveData) {
-  //     if (vRedirector.newVRouterData!.queryParameters.isNotEmpty) {
-  //       redirectParm['redirect'] = 'true';
-  //       redirectParm['redirectPath'] = vRedirector.newVRouterData!.url!;
-  //     }
-  //   }
-  //   if (!_isLogged!) {
-  //     debugPrint("onPrivateUpdate with user not logged in");
-  //     ErrorHandler.logError(
-  //       e: Exception("onPrivateUpdate with user not logged in"),
-  //       s: StackTrace.current,
-  //     );
-  //     // vRedirector.to(defaultRoute, queryParameters: redirectParm);
-  //   } else {
-  //     if (pController != null) {
-  //       if (!await pController!
-  //           .userController.isUserDataAvailableAndDateOfBirthSet) {
-  //         debugPrint("reroute to user_age");
-  //         vRedirector.to(
-  //           '/home/connect/user_age',
-  //           queryParameters: redirectParm,
-  //         );
-  //       }
-  // } else {
-  //   debugPrint("controller is null in pguard check");
-  // }
-  //   }
-
-  //   isPublicLeaving = false;
-  //   return;
-  // }
 }
