4f04b0bed0346e1ca93097b4391dfb133c9a2234
Kelrap <kel.raphael3@outlook.com>
2025-10-03 09:16:59 -0400

    Limit height instead of hardcoding values

--- lib/pages/chat/chat_view.dart
+++ lib/pages/chat/chat_view.dart
@@ -243,18 +243,21 @@ class ChatView extends StatelessWidget {
                                     .where(
                                       (syncUpdate) => syncUpdate.hasRoomUpdate,
                                     ),
-                                builder: (context, _) => UnreadRoomsBadge(
-                                  filter: (r) => r.id != controller.roomId,
-                                  badgePosition: BadgePosition.topEnd(
-                                    end: 8,
-                                    // #Pangea
-                                    // top: 4
-                                    top: controller.room.showActivityChatUI
-                                        ? 29
-                                        : 4,
-                                    // Pangea#
+                                // #Pangea
+                                // builder: (context, _) => UnreadRoomsBadge(
+                                builder: (context, _) => Center(
+                                  child: SizedBox(
+                                    height: kToolbarHeight,
+                                    child: UnreadRoomsBadge(
+                                      // Pangea#
+                                      filter: (r) => r.id != controller.roomId,
+                                      badgePosition: BadgePosition.topEnd(
+                                        end: 8,
+                                        top: 4,
+                                      ),
+                                      child: const Center(child: BackButton()),
+                                    ),
                                   ),
-                                  child: const Center(child: BackButton()),
                                 ),
                               ),
                 titleSpacing: FluffyThemes.isColumnMode(context) ? 24 : 0,
