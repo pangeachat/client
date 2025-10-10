a46810c38c262fd67396cc38786e2446f44318d6
ggurdin <ggurdin@gmail.com>
2025-10-10 09:43:57 -0400

    exclude bot in invite page

--- lib/pangea/chat_settings/utils/bot_client_extension.dart
+++ lib/pangea/chat_settings/utils/bot_client_extension.dart
@@ -4,7 +4,9 @@ import 'package:fluffychat/pangea/bot/utils/bot_name.dart';
 import 'package:fluffychat/pangea/chat/constants/default_power_level.dart';
 import 'package:fluffychat/pangea/chat_settings/constants/bot_mode.dart';
 import 'package:fluffychat/pangea/chat_settings/models/bot_options_model.dart';
+import 'package:fluffychat/pangea/events/constants/pangea_event_types.dart';
 import 'package:fluffychat/pangea/extensions/pangea_room_extension.dart';
+import 'package:fluffychat/widgets/matrix.dart';
 
 extension BotClientExtension on Client {
   bool get hasBotDM => rooms.any((room) {
@@ -22,7 +24,16 @@ extension BotClientExtension on Client {
         BotName.byEnvironment,
         preset: CreateRoomPreset.trustedPrivateChat,
         initialState: [
-          BotOptionsModel(mode: BotMode.directChat).toStateEvent,
+          StateEvent(
+            content: BotOptionsModel(
+              mode: BotMode.directChat,
+              targetLanguage: MatrixState
+                  .pangeaController.languageController.userL2?.langCode,
+              languageLevel: MatrixState.pangeaController.userController.profile
+                  .userSettings.cefrLevel,
+            ).toJson(),
+            type: PangeaEventTypes.botOptions,
+          ),
           RoomDefaults.defaultPowerLevels(
             userID!,
           ),
