81a713fbad0a7aac7ac79fdc70f79d6ff9a7e9fe
Kelrap <kel.raphael3@outlook.com>
2025-10-30 11:59:58 -0400

    Change save activity button text and behavior

--- lib/pangea/activity_sessions/activity_session_chat/activity_finished_status_message.dart
+++ lib/pangea/activity_sessions/activity_session_chat/activity_finished_status_message.dart
@@ -8,7 +8,6 @@ import 'package:fluffychat/pages/chat/chat.dart';
 import 'package:fluffychat/pangea/activity_sessions/activity_room_extension.dart';
 import 'package:fluffychat/pangea/activity_summary/activity_summary_model.dart';
 import 'package:fluffychat/pangea/common/widgets/error_indicator.dart';
-import 'package:fluffychat/widgets/future_loading_dialog.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 
 class ActivityFinishedStatusMessage extends StatelessWidget {
@@ -19,31 +18,7 @@ class ActivityFinishedStatusMessage extends StatelessWidget {
     required this.controller,
   });
 
-  Future<void> _onArchive(BuildContext context) async {
-    {
-      final resp = await showFutureLoadingDialog(
-        context: context,
-        future: () => _archiveToAnalytics(context),
-      );
-
-      if (!resp.isError) {
-        context.push(
-          "/rooms/spaces/${controller.room.courseParent!.id}/details?tab=course",
-        );
-      }
-    }
-  }
-
-  Future<void> _archiveToAnalytics(BuildContext context) async {
-    await controller.room.archiveActivity();
-    await MatrixState.pangeaController.putAnalytics
-        .sendActivityAnalytics(controller.room.id);
-  }
-
   ActivitySummaryModel? get summary => controller.room.activitySummary;
-
-  bool get _enableArchive =>
-      summary?.summary != null || summary?.hasError == true;
 
   @override
   Widget build(BuildContext context) {
@@ -120,39 +95,37 @@ class ActivityFinishedStatusMessage extends StatelessWidget {
                           child: Text(L10n.of(context).requestSummaries),
                         ),
                       ],
-                      if (!controller.room.hasArchivedActivity) ...[
-                        Text(
-                          L10n.of(context).saveActivityDesc,
-                          style: const TextStyle(
-                            fontStyle: FontStyle.italic,
-                          ),
-                          textAlign: TextAlign.center,
+                      Text(
+                        L10n.of(context).saveActivityDesc,
+                        style: const TextStyle(
+                          fontStyle: FontStyle.italic,
                         ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 12.0,
-                              vertical: 8.0,
+                        textAlign: TextAlign.center,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12.0,
+                            vertical: 8.0,
+                          ),
+                          foregroundColor: theme.colorScheme.onPrimaryContainer,
+                          backgroundColor: theme.colorScheme.primaryContainer,
+                        ),
+                        onPressed: () => context.push(
+                          "/rooms/spaces/${controller.room.courseParent!.id}/details?tab=course",
+                        ),
+                        child: Row(
+                          spacing: 12.0,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.radar, size: 20.0),
+                            Text(
+                              L10n.of(context).nextActivityTitle,
+                              style: const TextStyle(fontSize: 12.0),
                             ),
-                            foregroundColor:
-                                theme.colorScheme.onPrimaryContainer,
-                            backgroundColor: theme.colorScheme.primaryContainer,
-                          ),
-                          onPressed:
-                              _enableArchive ? () => _onArchive(context) : null,
-                          child: Row(
-                            spacing: 12.0,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(Icons.radar, size: 20.0),
-                              Text(
-                                L10n.of(context).saveActivityTitle,
-                                style: const TextStyle(fontSize: 12.0),
-                              ),
-                            ],
-                          ),
+                          ],
                         ),
-                      ],
+                      ),
                     ]
                   : [
                       Text(
