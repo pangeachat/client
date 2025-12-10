43014b3207ec56f02b8225c340b7d65b78c0bddd
ggurdin <ggurdin@gmail.com>
2025-12-10 16:05:14 -0500

    chore: simplify progress bar widget

---
+++ lib/pangea/analytics_summary/progress_bar/animated_progress_bar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/config/themes.dart';
+
+class AnimatedProgressBar extends StatelessWidget {
+  final double height;
+  final double widthPercent;
+
+  final Color barColor;
+  final Color backgroundColor;
+  final Duration? duration;
+
+  const AnimatedProgressBar({
+    required this.height,
+    required this.widthPercent,
+    required this.barColor,
+    required this.backgroundColor,
+    this.duration,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Stack(
+          alignment: Alignment.centerLeft,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 2),
+              child: Container(
+                height: height,
+                width: constraints.maxWidth,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(AppConfig.borderRadius),
+                  ),
+                  color: backgroundColor,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 2),
+              child: AnimatedContainer(
+                duration: duration ?? FluffyThemes.animationDuration,
+                height: height,
+                width: constraints.maxWidth * widthPercent,
+                decoration: BoxDecoration(
+                  color: barColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(AppConfig.borderRadius),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
