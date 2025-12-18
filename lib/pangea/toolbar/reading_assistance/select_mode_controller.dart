e9e4604aad6269f9c9d43e67e9b416ff3fbbd688
ggurdin <ggurdin@gmail.com>
2025-12-18 14:48:51 -0500

    feat: While audio is playing, allow clicking of word to move audio to that spot

--- lib/pangea/toolbar/reading_assistance/select_mode_controller.dart
+++ lib/pangea/toolbar/reading_assistance/select_mode_controller.dart
@@ -10,6 +10,7 @@ import 'package:fluffychat/pangea/analytics_misc/lemma_emoji_setter_mixin.dart';
 import 'package:fluffychat/pangea/common/utils/async_state.dart';
 import 'package:fluffychat/pangea/constructs/construct_identifier.dart';
 import 'package:fluffychat/pangea/events/event_wrappers/pangea_message_event.dart';
+import 'package:fluffychat/pangea/events/models/pangea_token_text_model.dart';
 import 'package:fluffychat/pangea/speech_to_text/speech_to_text_response_model.dart';
 import 'package:fluffychat/pangea/toolbar/message_practice/message_audio_card.dart';
 import 'package:fluffychat/pangea/toolbar/reading_assistance/select_mode_buttons.dart';
@@ -92,10 +93,13 @@ class SelectModeController with LemmaEmojiSetter {
       ValueNotifier<(ConstructIdentifier, String)?>(null);
 
   final StreamController contentChangedStream = StreamController.broadcast();
+  ValueNotifier<PangeaTokenText?> playTokenNotifier =
+      ValueNotifier<PangeaTokenText?>(null);
 
   void dispose() {
     selectedMode.dispose();
     constructEmojiNotifier.dispose();
+    playTokenNotifier.dispose();
     _transcriptLoader.dispose();
     _translationLoader.dispose();
     _sttTranslationLoader.dispose();
@@ -196,6 +200,9 @@ class SelectModeController with LemmaEmojiSetter {
     String emoji,
   ) =>
       constructEmojiNotifier.value = (constructId, emoji);
+
+  void setPlayingToken(PangeaTokenText? token) =>
+      playTokenNotifier.value = token;
 
   Future<void> fetchAudio() => _audioLoader.load();
   Future<void> fetchTranslation() => _translationLoader.load();
