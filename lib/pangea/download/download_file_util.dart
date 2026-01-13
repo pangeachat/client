8ce368922bb54ad6aca3533820f89a35c016d77b
ggurdin <46800240+ggurdin@users.noreply.github.com>
2026-01-13 15:20:20 -0500

    chore: update download dialog copy to indicate that users should enable downloads on web (#5189)

--- lib/pangea/download/download_file_util.dart
+++ lib/pangea/download/download_file_util.dart
@@ -7,7 +7,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:universal_html/html.dart' as webfile;
 
-import 'package:fluffychat/pangea/common/utils/error_handler.dart';
+import 'package:fluffychat/pages/chat/recording_dialog.dart';
 import 'package:fluffychat/pangea/download/download_type_enum.dart';
 
 class DownloadUtil {
@@ -25,35 +25,32 @@ class DownloadUtil {
         ..click();
       return;
     }
+
+    final allowed = await Permission.storage.request().isGranted;
+    if (!allowed) {
+      throw PermissionException();
+    }
+
     if (await Permission.storage.request().isGranted) {
       Directory? directory;
-      try {
-        if (Platform.isIOS) {
-          directory = await getApplicationDocumentsDirectory();
-        } else {
-          directory = Directory('/storage/emulated/0/Download');
-          if (!await directory.exists()) {
-            directory = await getExternalStorageDirectory();
-          }
+
+      if (Platform.isIOS) {
+        directory = await getApplicationDocumentsDirectory();
+      } else {
+        directory = Directory('/storage/emulated/0/Download');
+        if (!await directory.exists()) {
+          directory = await getExternalStorageDirectory();
         }
-      } catch (err, s) {
-        debugPrint("Failed to get download folder path");
-        ErrorHandler.logError(
-          e: err,
-          s: s,
-          data: {},
-        );
       }
-      if (directory != null) {
-        final File f = File("${directory.path}/$filename");
-        File resp;
-        if (fileType == DownloadType.txt || fileType == DownloadType.csv) {
-          resp = await f.writeAsString(contents);
-        } else {
-          resp = await f.writeAsBytes(contents);
-        }
-        OpenFile.open(resp.path);
+
+      final File f = File("${directory!.path}/$filename");
+      File resp;
+      if (fileType == DownloadType.txt || fileType == DownloadType.csv) {
+        resp = await f.writeAsString(contents);
+      } else {
+        resp = await f.writeAsBytes(contents);
       }
+      OpenFile.open(resp.path);
     }
   }
 }
