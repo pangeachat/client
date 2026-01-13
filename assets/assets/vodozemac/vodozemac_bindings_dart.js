8e58cbb03b6a444bb473d89a12ced92697746fac
ggurdin <ggurdin@users.noreply.github.com>
2026-01-13 19:01:41 +0000

    deploy: c3c9788962131be7caf611303abcc2923b3186ea

--- assets/assets/vodozemac/vodozemac_bindings_dart.js
+++ assets/assets/vodozemac/vodozemac_bindings_dart.js
@@ -1595,7 +1595,7 @@ let wasm_bindgen;
     };
 
     function __wbg_adapter_40(arg0, arg1, arg2) {
-        wasm.closure578_externref_shim(arg0, arg1, arg2);
+        wasm.closure579_externref_shim(arg0, arg1, arg2);
     }
 
     const WorkerPoolFinalization = (typeof FinalizationRegistry === 'undefined')
@@ -1955,8 +1955,8 @@ let wasm_bindgen;
             const ret = false;
             return ret;
         };
-        imports.wbg.__wbindgen_closure_wrapper1707 = function(arg0, arg1, arg2) {
-            const ret = makeMutClosure(arg0, arg1, 579, __wbg_adapter_40);
+        imports.wbg.__wbindgen_closure_wrapper1708 = function(arg0, arg1, arg2) {
+            const ret = makeMutClosure(arg0, arg1, 580, __wbg_adapter_40);
             return ret;
         };
         imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
