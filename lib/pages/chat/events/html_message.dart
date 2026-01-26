c567cc27f2317bf8ec66ea51c6b87f5c919c81c5
ggurdin <ggurdin@gmail.com>
2026-01-26 13:41:58 -0500

    chore: If link, treat as regular message

--- lib/pages/chat/events/html_message.dart
+++ lib/pages/chat/events/html_message.dart
@@ -174,8 +174,9 @@ class HtmlMessage extends StatelessWidget {
       );
 
   String _addTokenTags() {
-    final regex = RegExp(r'(<[^>]+>)');
+    if (html.contains("<a href")) return html;
 
+    final regex = RegExp(r'(<[^>]+>)');
     final matches = regex.allMatches(html);
     final List<String> result = <String>[];
     int lastEnd = 0;
@@ -208,6 +209,8 @@ class HtmlMessage extends StatelessWidget {
         );
       }
     }
+
+    debugPrint("Results: $result");
 
     int position = 0;
     final tokenPositions = tokens != null
