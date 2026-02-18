9f4e97e1aa2df5b0ad25c1d8f5b6255c8a396dd2
ggurdin <ggurdin@gmail.com>
2026-02-18 10:33:27 -0500

    add mobile-size images

--- lib/pangea/login/pages/login_or_signup_view.dart
+++ lib/pangea/login/pages/login_or_signup_view.dart
@@ -10,10 +10,8 @@ import 'package:xml/xml.dart';
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/config/setting_keys.dart';
 import 'package:fluffychat/l10n/l10n.dart';
-import 'package:fluffychat/pangea/common/config/environment.dart';
 import 'package:fluffychat/pangea/common/widgets/pangea_logo_svg.dart';
 import 'package:fluffychat/pangea/join_codes/space_code_repo.dart';
-import 'package:fluffychat/pangea/login/widgets/app_config_dialog.dart';
 
 class LoginOrSignupView extends StatefulWidget {
   const LoginOrSignupView({super.key});
@@ -23,35 +21,39 @@ class LoginOrSignupView extends StatefulWidget {
 }
 
 class LoginOrSignupViewState extends State<LoginOrSignupView> {
+  bool _isMobile = false;
   final CarouselSliderController _controller = CarouselSliderController();
-  List<String> get _imageFileNames =>
-      List.generate(6, (index) => 'Carousel_${index + 1}_ratio2x1.svg');
 
-  List<AppConfigOverride> _overrides = [];
+  List<String> get _imageFileNames {
+    final ratioString = _isMobile ? 'ratio4x5' : 'ratio2x1';
+    return List.generate(
+      6,
+      (index) => 'Carousel_${index + 1}_$ratioString.svg',
+    );
+  }
+
+  // List<AppConfigOverride> _overrides = [];
   List<Future> _imageFutures = [];
 
   @override
   void initState() {
     super.initState();
-    _loadOverrides();
-    _imageFutures = _imageFileNames.mapIndexed((index, filename) async {
-      final resp = await http.get(
-        Uri.parse('${AppConfig.assetsBaseURL}/$filename'),
-      );
-      if (resp.statusCode != 200) {
-        throw Exception('Failed to load image: $filename');
-      }
-
-      final svg = resp.body;
-      final entry = _updatedIDs[index + 1];
-      if (entry != null) {
-        return updateSvgText(rawSvg: svg, replacements: entry);
-      }
-      return resp.body;
-    }).toList();
+    // _loadOverrides();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _isMobile = MediaQuery.widthOf(context) <= 832.0;
+      _updateImageFutures();
+    });
   }
 
   String? get _cachedSpaceCode => SpaceCodeRepo.spaceCode;
+
+  void _updateImageFutures() {
+    if (mounted) {
+      setState(() {
+        _imageFutures = _imageFileNames.mapIndexed(_loadSVG).toList();
+      });
+    }
+  }
 
   String updateSvgText({
     required String rawSvg,
@@ -88,32 +90,52 @@ class LoginOrSignupViewState extends State<LoginOrSignupView> {
     return document.toXmlString();
   }
 
-  Future<void> _loadOverrides() async {
-    final overrides = await Environment.getAppConfigOverrides();
-    if (mounted) {
-      setState(() => _overrides = overrides);
-    }
-  }
-
-  Future<void> _setEnvironment() async {
-    if (_overrides.isEmpty) return;
-
-    final resp = await showDialog<AppConfigOverride?>(
-      context: context,
-      builder: (context) => AppConfigDialog(overrides: _overrides),
+  Future<String> _loadSVG(int index, String filename) async {
+    final resp = await http.get(
+      Uri.parse('${AppConfig.assetsBaseURL}/$filename'),
     );
+    if (resp.statusCode != 200) {
+      throw Exception('Failed to load image: $filename');
+    }
 
-    await Environment.setAppConfigOverride(resp);
-    setState(() {});
+    final svg = resp.body;
+    final entry = _updatedIDs[index + 1];
+    if (entry != null) {
+      return updateSvgText(rawSvg: svg, replacements: entry);
+    }
+    return resp.body;
   }
+
+  // Future<void> _loadOverrides() async {
+  //   final overrides = await Environment.getAppConfigOverrides();
+  //   if (mounted) {
+  //     setState(() => _overrides = overrides);
+  //   }
+  // }
+
+  // Future<void> _setEnvironment() async {
+  //   if (_overrides.isEmpty) return;
+
+  //   final resp = await showDialog<AppConfigOverride?>(
+  //     context: context,
+  //     builder: (context) => AppConfigDialog(overrides: _overrides),
+  //   );
+
+  //   await Environment.setAppConfigOverride(resp);
+  //   setState(() {});
+  // }
 
   Map<int, Map<String, String>> get _updatedIDs => {
     1: {
       'Edit text': L10n.of(context).appDescription,
       'Edit text header': L10n.of(context).shareYourHobbies,
     },
-    2: {'Edit text Header': L10n.of(context).pangeaBot},
+    2: {
+      'Edit text Header': L10n.of(context).pangeaBot,
+      'Edit text': L10n.of(context).writeAndSpeakWorryFree,
+    },
     3: {
+      'Edit text': L10n.of(context).joinLearningCommunities,
       'Edit text_2': L10n.of(context).joinWithClassCode,
       'Edit text_4': L10n.of(context).startYourOwn,
     },
@@ -121,28 +143,28 @@ class LoginOrSignupViewState extends State<LoginOrSignupView> {
       'Edit text': L10n.of(context).playConversationGames,
       'Edit text_2': L10n.of(context).guessMyHometown,
     },
-    5: {'Edit text_2': L10n.of(context).languageExchange},
-    6: {},
+    5: {
+      'Edit text': L10n.of(context).jumpIntoConversation,
+      'Edit text_2': L10n.of(context).languageExchange,
+    },
+    6: {'Edit text': L10n.of(context).playPersonalizedGames},
   };
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final screenWidth = MediaQuery.widthOf(context);
-    final isMobile = screenWidth <= 832.0;
-
     final title = Row(
       spacing: 12.0,
       mainAxisSize: .min,
       children: [
         PangeaLogoSvg(
-          width: isMobile ? 32.0 : 56.0,
+          width: _isMobile ? 32.0 : 56.0,
           forceColor: theme.colorScheme.onSurface,
         ),
         Text(
           AppSettings.applicationName.value,
           style:
-              (isMobile
+              (_isMobile
                       ? theme.textTheme.headlineSmall
                       : theme.textTheme.displayMedium)
                   ?.copyWith(fontWeight: FontWeight.bold),
@@ -161,14 +183,20 @@ class LoginOrSignupViewState extends State<LoginOrSignupView> {
       //         ],
       //       )
       //     : null,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: FutureBuilder(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final isMobile = constraints.maxWidth <= 832.0;
+
+          // Only regenerate if breakpoint changes
+          if (_isMobile != isMobile) {
+            _isMobile = isMobile;
+            _updateImageFutures();
+          }
+
+          return SafeArea(
+            child: Column(
+              children: [
+                FutureBuilder(
                   future: Future.wait(_imageFutures),
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
@@ -177,88 +205,130 @@ class LoginOrSignupViewState extends State<LoginOrSignupView> {
                       return SizedBox();
                     }
 
-                    return CarouselSlider(
-                      items: snapshot.data!
-                          .map(
-                            (item) => SvgPicture.string(
-                              item,
-                              placeholderBuilder: (context) => const Center(
-                                child: CircularProgressIndicator(),
+                    return AspectRatio(
+                      aspectRatio: _isMobile ? 4 / 5 : 2 / 1,
+                      child: CarouselSlider(
+                        items: snapshot.data!
+                            .map(
+                              (item) => SizedBox.expand(
+                                child: SvgPicture.string(
+                                  item,
+                                  fit: BoxFit.contain, // 🔥 important change
+                                  alignment: Alignment.topCenter,
+                                ),
                               ),
-                            ),
-                          )
-                          .toList(),
-                      carouselController: _controller,
-                      options: CarouselOptions(
-                        enlargeCenterPage: true,
-                        autoPlay: true,
+                            )
+                            .toList(),
+                        carouselController: _controller,
+                        options: CarouselOptions(
+                          height: double.infinity,
+                          viewportFraction: _isMobile ? 1.0 : 0.8,
+                          enlargeCenterPage: !_isMobile,
+                          autoPlay: true,
+                        ),
                       ),
                     );
                   },
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Center(
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 500),
-                  child: Column(
-                    spacing: isMobile ? 8.0 : 16.0,
-                    mainAxisAlignment: .center,
-                    children: [
-                      title,
-                      ElevatedButton(
-                        // push instead of go so the app bar back button doesn't go to the language selection page
-                        // https://github.com/pangeachat/client/issues/4421
-                        onPressed: () => context.push(
-                          _cachedSpaceCode != null
-                              ? '/home/language/signup'
-                              : '/home/language',
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: theme.colorScheme.primaryContainer,
-                          foregroundColor: theme.colorScheme.onPrimaryContainer,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              L10n.of(context).getStarted,
-                              style: TextStyle(fontWeight: FontWeight.bold),
+                // Expanded(
+                //   flex: 2,
+                // child: FutureBuilder(
+                //   future: Future.wait(_imageFutures),
+                //   builder: (context, snapshot) {
+                //     if (snapshot.connectionState == ConnectionState.waiting) {
+                //       return const Center(child: CircularProgressIndicator());
+                //     } else if (snapshot.hasError) {
+                //       return SizedBox();
+                //     }
+
+                //     return CarouselSlider(
+                //         items: snapshot.data!
+                //             .map(
+                //               (item) => SizedBox.expand(
+                //                 child: SvgPicture.string(
+                //                   item,
+                //                   fit: BoxFit.cover, // 🔥 important
+                //                   alignment: Alignment.center,
+                //                 ),
+                //               ),
+                //             )
+                //             .toList(),
+                //         carouselController: _controller,
+                //         options: CarouselOptions(
+                //           height: double.infinity,
+                //           enlargeCenterPage: !_isMobile,
+                //           autoPlay: true,
+                //           viewportFraction: _isMobile ? 1.0 : 0.8,
+                //         ),
+                //       );
+                //     },
+                //   ),
+                // ),
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 500),
+                      child: Column(
+                        spacing: isMobile ? 8.0 : 16.0,
+                        mainAxisAlignment: .center,
+                        children: [
+                          title,
+                          ElevatedButton(
+                            // push instead of go so the app bar back button doesn't go to the language selection page
+                            // https://github.com/pangeachat/client/issues/4421
+                            onPressed: () => context.push(
+                              _cachedSpaceCode != null
+                                  ? '/home/language/signup'
+                                  : '/home/language',
                             ),
-                          ],
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () => context.go('/home/login'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: theme.colorScheme.surface,
-                          foregroundColor: theme.colorScheme.onSurface,
-                          shadowColor: Colors.transparent,
-                          overlayColor: Colors.transparent,
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 16,
-                            vertical: 0,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  theme.colorScheme.primaryContainer,
+                              foregroundColor:
+                                  theme.colorScheme.onPrimaryContainer,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  L10n.of(context).getStarted,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              L10n.of(context).loginToAccount,
-                              style: TextStyle(fontWeight: FontWeight.bold),
+                          ElevatedButton(
+                            onPressed: () => context.go('/home/login'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: theme.colorScheme.surface,
+                              foregroundColor: theme.colorScheme.onSurface,
+                              shadowColor: Colors.transparent,
+                              overlayColor: Colors.transparent,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 0,
+                              ),
                             ),
-                          ],
-                        ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  L10n.of(context).loginToAccount,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
