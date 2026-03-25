693a3852db5b53152d76f142f3e5d779720f630c
ggurdin <ggurdin@gmail.com>
2026-03-25 13:30:00 -0400

    feat: use language SVGs instead of flag emojis

--- lib/pangea/languages/language_model.dart
+++ lib/pangea/languages/language_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:collection/collection.dart';
 
+import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/l10n/l10n.dart';
 import 'package:fluffychat/pangea/languages/l2_support_enum.dart';
 import 'package:fluffychat/pangea/languages/language_constants.dart';
@@ -302,11 +303,7 @@ class LanguageModel {
       "zuDisplayName": l10n.zuDisplayName,
     };
 
-    final display = displayNameMap[langKey] ?? displayName;
-    if (langCode.contains('-') && localeEmoji != null) {
-      return "$display $localeEmoji";
-    }
-    return display;
+    return displayNameMap[langKey] ?? displayName;
   }
 
   String get langCodeShort => langCode.split('-').first;
@@ -320,6 +317,9 @@ class LanguageModel {
   TextDirection get textDirection {
     return _textDirection ?? _defaultTextDirection;
   }
+
+  Uri get svgUrl =>
+      Uri.parse("${AppConfig.assetsBaseURL}/language-flags/$langCode.svg");
 
   static bool search(
     LanguageModel? item,
