fd377163b164bffa92a44db01172656c743c2f72
Kelrap <kel.raphael3@outlook.com>
2026-05-22 11:28:22 -0400

    Remove hardcoding on chat list navigation flow

--- e2e/scripts/chat-list-navigation.spec.ts
+++ e2e/scripts/chat-list-navigation.spec.ts
@@ -12,12 +12,12 @@ import { expect, test } from "../fixtures";
  * - lib/config/routes.dart
  */
 
-// playwright todo: update assumptions
-// Context assumptions:
-// There is a chat named 'playwright' in the chat list
+// Prerequisites:
+// L1 is english
+// There is at least 1 joined course
 test.describe("Chat list navigation", () => {
 
-  test("should be move to, from, and within the chat list page", async ({
+  test("should be able to move to, from, and within the chat list page", async ({
     page,
   }) => {
 
@@ -26,16 +26,35 @@ test.describe("Chat list navigation", () => {
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const intl = JSON.parse(fileContent);
 
-    // Navigate to home page
+    // Navigate to home page and check that it works
+    // Then return to normal chat list page
     await page.getByRole("button", { name: intl.home, exact: true }).click();
     await expect(page.getByText(intl.profile, { exact: true })).toBeVisible();
     await page.getByRole("button", { name: intl.allChats, exact: true }).click();
 
-    // Navigate to/from a course
-    // playwright todo: hardcoded course name
-    await page.getByRole("button", { name: "Global Connections" }).click();
-    await page.getByRole("button", { name: intl.skipForNow, exact: true }).click();
-    await expect(page.getByRole("button", { name: intl.introChatTitle })).toBeVisible();
+    // Find all cousin buttons of addCourse button
+    // Then filter out non-course buttons
+    var nonCourse = [intl.addCourse, intl.allChats, intl.learningAnalytics, intl.home];
+    var nonCourseFilter = new RegExp(`\\b(${nonCourse.join('|')})\\b`);
+    var courses = page.getByRole("button", { name: intl.addCourse }).locator('..').locator('..').getByRole("button").filter({ hasNotText: nonCourseFilter });
+
+    // If no courses are found, test requirements not met
+    await expect(await courses.count(), { message: 'Account must have 1+ joined courses' }).toBeGreaterThan(0);
+
+    // Navigate to first course in course list
+    await courses.first().click();
+
+    // If notification request button appears, close it
+    if (await page.getByRole("button", { name: intl.skipForNow, exact: true }).isVisible()) {
+      await page.getByRole("button", { name: intl.skipForNow, exact: true }).click();
+    }
+
+    // Check that course page shows properly
+    // playwright todo: If using this script for mobile testing, 
+    // will need to use elements in course chat list to check instead
+    await expect(page.getByRole("button", { name: intl.coursePlan })).toBeVisible();
+
+    // Return to normal chat list page
     await page.getByRole("button", { name: intl.allChats, exact: true }).click();
 
     // Show learning settings popup
