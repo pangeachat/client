fa987f6e42e9075a24e2cbd9da2043c66312dc96
Kelrap <99418823+Kelrap@users.noreply.github.com>
2026-06-03 16:03:35 -0400

    When press small 'im done' button, wrap up activity (#6845)

--- lib/pangea/activity_orchestrator/activity_stats_menu.dart
+++ lib/pangea/activity_orchestrator/activity_stats_menu.dart
@@ -47,13 +47,13 @@ class ActivityStatsMenu extends StatelessWidget {
   bool get _showEndForAll =>
       !_activityComplete && room.isRoomAdmin && !_isTwoPersonBotActivity;
 
-  Future<void> _finishActivityForMe(BuildContext context) async {
+  Future<void> _finishActivityForMe(BuildContext context, bool close) async {
     final resp = await showFutureLoadingDialog(
       context: context,
       future: room.finishActivity,
     );
 
-    if (!resp.isError) {
+    if (close && !resp.isError) {
       toggleVisibility();
     }
   }
@@ -121,9 +121,11 @@ class ActivityStatsMenu extends StatelessWidget {
                             ),
                             if (!_activityComplete &&
                                 room.hasCompletedAllGoals &&
-                                !showDropdown)
+                                !showDropdown &&
+                                !room.hasCompletedRole)
                               InkWell(
-                                onTap: toggleVisibility,
+                                onTap: () =>
+                                    _finishActivityForMe(context, false),
                                 child: Container(
                                   padding: EdgeInsets.symmetric(
                                     vertical: 6.0,
@@ -208,7 +210,7 @@ class ActivityStatsMenu extends StatelessWidget {
                       ),
                     if (_showEndForMe)
                       ElevatedButton(
-                        onPressed: () => _finishActivityForMe(context),
+                        onPressed: () => _finishActivityForMe(context, true),
                         style: ElevatedButton.styleFrom(
                           backgroundColor: room.hasCompletedAllGoals
                               ? theme.brightness == Brightness.light
