41d14b54c2de374b68fd0007bc0af56d29c353f0
ggurdin <ggurdin@gmail.com>
2026-06-09 12:40:32 -0400

    chore: update styling of end for all button when all users have completed their activity goals

--- lib/pangea/activity_orchestrator/orchestrator_room_extension.dart
+++ lib/pangea/activity_orchestrator/orchestrator_room_extension.dart
@@ -3,6 +3,8 @@ import 'package:matrix/matrix.dart';
 import 'package:fluffychat/pangea/activity_orchestrator/orchestrator_awarded_goals.dart';
 import 'package:fluffychat/pangea/activity_sessions/activity_plan_model.dart';
 import 'package:fluffychat/pangea/activity_sessions/activity_roles_room_extension.dart';
+import 'package:fluffychat/pangea/activity_sessions/activity_room_extension.dart';
+import 'package:fluffychat/pangea/bot/utils/bot_name.dart';
 import 'package:fluffychat/pangea/events/constants/pangea_event_types.dart';
 
 extension OrchestratorRoomExtension on Room {
@@ -16,18 +18,24 @@ extension OrchestratorRoomExtension on Room {
     }
   }
 
-  bool isGoalCompleted(String id) {
+  bool isOwnGoalCompleted(String id) {
     final ownRole = this.ownRole;
     if (ownRole == null) return false;
     return orchestratorAwardedGoals.isGoalCompleted(id);
   }
 
-  bool get hasCompletedAllGoals {
+  bool get hasCompletedOwnGoals {
     final ownRole = this.ownRole;
     if (ownRole == null) return false;
+    return hasCompletedGoalsByRoleId(ownRole.id);
+  }
 
-    final allGoals = ownRole.allGoals;
-    return allGoals.every((r) => isGoalCompleted(r.id));
+  bool hasCompletedGoalsByRoleId(String roleId) {
+    final role = activityPlan?.roles[roleId];
+    if (role == null) return false;
+    final goals = role.allGoals;
+    final completedGoals = orchestratorAwardedGoals.goalIds;
+    return goals.every((g) => completedGoals.contains(g.id));
   }
 
   List<ActivityRoleGoal> get ownCompletedGoals {
@@ -35,9 +43,21 @@ extension OrchestratorRoomExtension on Room {
     if (ownRole == null) return [];
 
     final ownGoals = ownRole.allGoals;
-    Logs().w("Own Goals: ${ownGoals.map((g) => g.toJson()).toList()}");
-    Logs().w("Awarded goals: ${orchestratorAwardedGoals.toJson()}");
     final awardedGoals = orchestratorAwardedGoals.goalIds;
     return ownGoals.where((g) => awardedGoals.contains(g.id)).toList();
+  }
+
+  bool get haveAllRolesCompletedAllGoals {
+    final roles = activityPlan?.roles;
+    final assignedRoles = activityRoles?.roles;
+    if (roles == null || assignedRoles == null) return false;
+
+    final completedRoles = roles.values.where((r) {
+      final assignedRole = assignedRoles[r.id];
+      return hasCompletedGoalsByRoleId(r.id) ||
+          assignedRole?.userId == BotName.byEnvironment;
+    });
+
+    return completedRoles.length >= roles.length;
   }
 }
