a9eb4600eea4448ff31d07e57a58c02bfecdb06d
ggurdin <46800240+ggurdin@users.noreply.github.com>
2026-06-29 16:29:04 -0400

    fix: remove title from construct details page headers (#7386)

--- lib/routes/world/right_panel/workspace_right_panel.dart
+++ lib/routes/world/right_panel/workspace_right_panel.dart
@@ -124,7 +124,7 @@ class WorkspaceRightPanel extends StatelessWidget {
               );
       }(),
       'vocab' || 'grammar' => PanelCardWithHeader(
-        title: _construct?.lemma ?? '',
+        title: '',
         icon: leadingIcon,
         onLeading: onLeading,
         tooltip: leadingTooltip,
