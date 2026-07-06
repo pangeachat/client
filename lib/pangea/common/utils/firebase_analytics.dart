6a8c465d5ffb350ba569b1fb505f2ae3bf39b6da
ggurdin <ggurdin@gmail.com>
2026-07-06 10:06:01 -0400

    chore: set environment user property on init Google Analytics

--- lib/pangea/common/utils/firebase_analytics.dart
+++ lib/pangea/common/utils/firebase_analytics.dart
@@ -59,6 +59,7 @@ class GoogleAnalytics {
     analytics = FirebaseAnalytics.instanceFor(app: app);
     // Client is not automatically set on web
     await _setClientVersion();
+    await _setEnvironment();
 
     debugPrint("Firebase App Name: ${app.name}");
     debugPrint("Firebase App Options:");
@@ -79,6 +80,13 @@ class GoogleAnalytics {
     } catch (error) {
       debugPrint('Unable to set analytics client version: $error');
     }
+  }
+
+  static Future<void> _setEnvironment() async {
+    await analytics?.setUserProperty(
+      name: 'environment',
+      value: Environment.isStagingEnvironment ? 'staging' : 'prod',
+    );
   }
 
   static Future<void> analyticsUserUpdate(String? userID) async {
