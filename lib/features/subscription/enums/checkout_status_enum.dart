939e344516bb32ec647d1db731e3ff96ca6535d1
ggurdin <ggurdin@gmail.com>
2026-07-17 09:42:26 -0400

    fallback to unknown checkout status type

--- lib/features/subscription/enums/checkout_status_enum.dart
+++ lib/features/subscription/enums/checkout_status_enum.dart
@@ -1,10 +1,14 @@
 enum CheckoutStatus {
   created,
   reused,
-  creating;
+  creating,
+  unknown;
 
   factory CheckoutStatus.fromJson(String value) {
-    return CheckoutStatus.values.firstWhere((e) => e.name == value);
+    return CheckoutStatus.values.firstWhere(
+      (e) => e.name == value,
+      orElse: () => CheckoutStatus.unknown,
+    );
   }
 
   String toJson() => name;
