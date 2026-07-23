07087766ad72c6e5907ce8ca37c8a04d60a5dabf
Kelrap <99418823+Kelrap@users.noreply.github.com>
2026-07-23 09:56:59 -0400

    Use PangeaSearchBar for add/browse course searching (#7878)
    Co-authored-by: ggurdin <ggurdin@gmail.com>

--- lib/widgets/pangea_search_bar.dart
+++ lib/widgets/pangea_search_bar.dart
@@ -28,46 +28,49 @@ class PangeaSearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Material(
-      elevation: 4,
-      borderRadius: BorderRadius.circular(99),
-      color: theme.colorScheme.surface,
-      child: Semantics(
-        container: true,
-        child: TextField(
-          textInputAction: TextInputAction.search,
-          controller: controller,
-          autofocus: autofocus,
-          focusNode: focusNode,
-          onChanged: onChanged,
-          onSubmitted: onSubmitted,
-          enabled: enabled,
-          decoration: InputDecoration(
-            isDense: true,
-            filled: true,
-            fillColor: theme.colorScheme.surface,
-            labelText: labelText,
-            prefixIcon: prefixIcon ?? const Icon(Icons.search),
-            suffixIcon: suffixIcon,
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(99),
-              borderSide: BorderSide(
-                color: theme.colorScheme.outlineVariant,
-                width: 1,
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(vertical: 4),
+      child: Material(
+        elevation: 4,
+        borderRadius: BorderRadius.circular(99),
+        color: theme.colorScheme.surface,
+        child: Semantics(
+          container: true,
+          child: TextField(
+            textInputAction: TextInputAction.search,
+            controller: controller,
+            autofocus: autofocus,
+            focusNode: focusNode,
+            onChanged: onChanged,
+            onSubmitted: onSubmitted,
+            enabled: enabled,
+            decoration: InputDecoration(
+              isDense: true,
+              filled: true,
+              fillColor: theme.colorScheme.surface,
+              labelText: labelText,
+              prefixIcon: prefixIcon ?? const Icon(Icons.search),
+              suffixIcon: suffixIcon,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(99),
+                borderSide: BorderSide(
+                  color: theme.colorScheme.outlineVariant,
+                  width: 1,
+                ),
               ),
-            ),
-            disabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(99),
-              borderSide: BorderSide(
-                color: theme.colorScheme.outlineVariant,
-                width: 1,
+              disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(99),
+                borderSide: BorderSide(
+                  color: theme.colorScheme.outlineVariant,
+                  width: 1,
+                ),
               ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(99),
-              borderSide: BorderSide(
-                color: theme.colorScheme.primary,
-                width: 2,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(99),
+                borderSide: BorderSide(
+                  color: theme.colorScheme.primary,
+                  width: 2,
+                ),
               ),
             ),
           ),
