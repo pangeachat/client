6c7911bb06c0d6a2dbd44eb1580e80affad43555
ggurdin <ggurdin@gmail.com>
2026-07-27 15:12:41 -0400

    fix: add back block semantics to activity list navigation arrows

--- lib/routes/courses/course_objectives/objective_section_scroll_arrow.dart
+++ lib/routes/courses/course_objectives/objective_section_scroll_arrow.dart
@@ -30,18 +30,20 @@ class ObjectiveSectionScrollArrow extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final l10n = L10n.of(context);
-    return Semantics(
-      button: true,
-      label: direction.label(l10n),
-      child: MouseRegion(
-        cursor: SystemMouseCursors.click,
-        child: GestureDetector(
-          onTap: onTap,
-          child: Container(
-            width: 40.0,
-            padding: EdgeInsets.all(8.0),
-            decoration: BoxDecoration(color: theme.colorScheme.surface),
-            child: Center(child: Icon(direction.icon)),
+    return BlockSemantics(
+      child: Semantics(
+        button: true,
+        label: direction.label(l10n),
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: onTap,
+            child: Container(
+              width: 40.0,
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(color: theme.colorScheme.surface),
+              child: Center(child: Icon(direction.icon)),
+            ),
           ),
         ),
       ),
