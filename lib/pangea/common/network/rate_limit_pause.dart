9d456ded978a2052906b6c1d70d40b6c4d74705a
ggurdin <ggurdin@gmail.com>
2026-08-20 10:03:42 -0400

    style: fix import sort order (Closes #8507)

--- lib/pangea/common/network/rate_limit_pause.dart
+++ lib/pangea/common/network/rate_limit_pause.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+
 import 'package:sentry_flutter/sentry_flutter.dart' show SentryLevel;
 
 import 'package:fluffychat/pangea/common/network/pangea_http_exception.dart';
